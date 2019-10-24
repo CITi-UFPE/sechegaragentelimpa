4c32c73e774bc73e8021f46e09379ea264d71dea
eulaliaaires <memgra@cin.ufpe.br>
2019-10-24 16:08:31 -0300

    remove comments

--- views/home/home.js
+++ views/home/home.js
@@ -8,31 +8,6 @@ $(document).ready(function(){
         speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
-        // responsive: [
-        //   {
-        //     breakpoint: 1024,
-        //     settings: {
-        //       slidesToShow: 3,
-        //       slidesToScroll: 3,
-        //       infinite: true,
-        //       dots: true
-        //     }
-        //   },
-        //   {
-        //     breakpoint: 600,
-        //     settings: {
-        //       slidesToShow: 2,
-        //       slidesToScroll: 2
-        //     }
-        //   },
-        //   {
-        //     breakpoint: 480,
-        //     settings: {
-        //       slidesToShow: 1,
-        //       slidesToScroll: 1
-        //     }
-        //   }
     });
 });
-          
 
