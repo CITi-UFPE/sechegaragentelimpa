1bed195c5e8f1bb7dcf7e38104690a49b181b8d1
Edgar Marques <edgarmarques.110@gmail.com>
2019-10-24 12:25:47 -0300

    Feature/facebook-api: Add get username and image from graph API

--- views/home/home.js
+++ views/home/home.js
@@ -1,5 +1,28 @@
-console.log('It works!');
+const gel = el => document.querySelector(el);
 
+window.fbAsyncInit = () => {
+    FB.init({
+      appId            : '1329423883914707',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v4.0'
+    });
+    gel('.fb-login-button').addEventListener('click', () => {
+        FB.login((res) => {
+            if (!res || !res.authResponse) return;
+            const id = res.authResponse.userID;
+            gel('.user-container').innerHTML += `
+                <img src="https://graph.facebook.com/${id}/picture?type=normal" />
+            `;
+            FB.api(id, (res) => {
+                gel('.user-container').innerHTML += `
+                    <h1>${res.name}</h1>
+                `;
+            })
+        })
+    });
+};
+// Slick
 $(document).ready(function(){
     $('.carousel').slick({
         arrows: false,
