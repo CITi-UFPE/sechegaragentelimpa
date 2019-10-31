a1c569fc09c5bcf4e1a421647659fb9b6f711073
Eder Marques <edermarques96@gmail.com>
2019-10-31 14:06:25 -0300

    change https redirect

--- app.js
+++ app.js
@@ -27,94 +27,134 @@ const getViewPath = view => path.join(__dirname, `views/${view}/${view}.html`);
 
 // ==================== DATABASE ==================== //
 firebase.initializeApp({
-    apiKey: "AIzaSyA9S4Wy6neP65df7tW3t-QejHbxDOsXfH8",
-    authDomain: "sechegaragentelimpa-9d9d0.firebaseapp.com",
-    databaseURL: "https://sechegaragentelimpa-9d9d0.firebaseio.com",
-    projectId: "sechegaragentelimpa-9d9d0",
-    storageBucket: "sechegaragentelimpa-9d9d0.appspot.com",
-    messagingSenderId: "372909306254",
-    appId: "1:372909306254:web:66b0febebba812852af4df",
-    measurementId: "G-YX93175YCL"
-  })
-  
-  const db = firebase.database();
+  apiKey: "AIzaSyA9S4Wy6neP65df7tW3t-QejHbxDOsXfH8",
+  authDomain: "sechegaragentelimpa-9d9d0.firebaseapp.com",
+  databaseURL: "https://sechegaragentelimpa-9d9d0.firebaseio.com",
+  projectId: "sechegaragentelimpa-9d9d0",
+  storageBucket: "sechegaragentelimpa-9d9d0.appspot.com",
+  messagingSenderId: "372909306254",
+  appId: "1:372909306254:web:66b0febebba812852af4df",
+  measurementId: "G-YX93175YCL"
+})
+
+const db = firebase.database();
 // ==================== ROUTES ==================== //
 
 // ==================== RENDER VIEWS ==================== //
 
+app.use((req, res, next) => {
+  try {
+    if (req.secure) {
+      next();
+      return;
+    }
+
+    res.redirect('https://' + req.headers.host + req.url);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
 app.get('/', (req, res) => {
+  try {
     res.sendFile(getViewPath('home'));
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 });
 
 app.post('/', async (req, res) => {
   try {
-      const key = req.body.lat.toString().replace('.', ',') + req.body.lat.toString().replace('.', ',')
-      if (req.body.remove) {
-          await db.ref('positions/').child(key).remove();
-          res.send('deleted');
-      } else {
-          await db.ref('positions/').child(key).set({
-              lat: req.body.lat,
-              long: req.body.long
-          });
-          res.send('created');
-      }
+    const key = req.body.lat.toString().replace('.', ',') + req.body.lat.toString().replace('.', ',')
+    if (req.body.remove) {
+      await db.ref('positions/').child(key).remove();
+      res.send('deleted');
+    } else {
+      await db.ref('positions/').child(key).set({
+        lat: req.body.lat,
+        long: req.body.long
+      });
+      res.send('created');
+    }
   } catch (err) {
-      console.log(err);
-      res.status(500).send(err);
+    console.log(err);
+    res.status(500).send(err);
   }
 });
 
 app.get('/politica-de-privacidade', (req, res) => {
+  try {
     res.sendFile(getViewPath('politica-de-privacidade'));
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 });
 
-app.post('/',(req,res) => {
+app.post('/', (req, res) => {
+  try {
     const key = req.body.lat.toString().replace('.', ',') + req.body.lat.toString().replace('.', ',')
-    if(req.body.remove){
-        db.ref('positions/').child(key).remove()
+    if (req.body.remove) {
+      db.ref('positions/').child(key).remove();
+      res.send('deleted');
     } else {
-        db.ref('positions/').child(key).set({
-             lat:req.body.lat,
-             long:req.body.long
-        });
+      db.ref('positions/').child(key).set({
+        lat: req.body.lat,
+        long: req.body.long
+      });
+      res.send('added');
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 });
 
 // db.ref('positions').set(''); 
 
-app.get('/positions', (req,res) => {
-    db.ref('positions/').once('value').then((snapshot) => {
-        res.json(snapshot.val());
-    });
+app.get('/positions', async (req, res) => {
+  try {
+    const snapshot = await db.ref('positions/').once('value');
+    res.json(snapshot.val());
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 });
 
 app.post('/user', async (req, res) => {
-    try {
-        const { 
-            id,
-            img,
-            name,
-        } = req.body;
-    
-        await db.ref('users/').child(id).set({
-            img,
-            name,
-        });
-    } catch (err) {
-        res.send(err);
-    }
+  try {
+    const {
+      id,
+      img,
+      name,
+    } = req.body;
+
+    await db.ref('users/').child(id).set({
+      img,
+      name,
+    });
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 app.get('/users', async (req, res) => {
+  try {
     const snapshot = await db.ref('users/').once('value');
     res.send(snapshot.val());
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 });
 
 // ==================== START SERVER ==================== //
 
 app.listen(process.env.PORT, () => {
-    console.log(`Running on port ${process.env.PORT}`);
+  console.log(`Running on port ${process.env.PORT}`);
 });
 
 // ====================================================== //
