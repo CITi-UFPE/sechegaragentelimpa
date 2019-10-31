166a35693740f757b3c1a089643735669b834b7f
Gabriel <gfsca@cin.ufpe.br>
2019-10-31 13:35:12 -0300

    app error fix

--- views/home/home.js
+++ views/home/home.js
@@ -1,28 +1,28 @@
 const gel = el => document.querySelector(el);
 
-window.fbAsyncInit = async () => {
-  FB.init({
-    appId: '1329423883914707',
-    autoLogAppEvents: true,
-    xfbml: true,
-    version: 'v1.0'
-  });
-  gel('.fb-login-button').addEventListener('click', () => {
-    FB.login((res) => {
-      if (!res || !res.authResponse) return;
-      const id = res.authResponse.userID;
-      const userImgSrc = `https://graph.facebook.com/${id}/picture?type=normal`;
-      FB.api(id, async (res) => {
-        const userName = res.name;
-        const response = await axios.post('/user', {
-          id: id,
-          img: userImgSrc,
-          name: userName,
-        });
-      });
-    });
-  });
-};
+// window.fbAsyncInit = async () => {
+//   FB.init({
+//     appId: '1329423883914707',
+//     autoLogAppEvents: true,
+//     xfbml: true,
+//     version: 'v1.0'
+//   });
+//   gel('.fb-login-button').addEventListener('click', () => {
+//     FB.login((res) => {
+//       if (!res || !res.authResponse) return;
+//       const id = res.authResponse.userID;
+//       const userImgSrc = `https://graph.facebook.com/${id}/picture?type=normal`;
+//       FB.api(id, async (res) => {
+//         const userName = res.name;
+//         const response = await axios.post('/user', {
+//           id: id,
+//           img: userImgSrc,
+//           name: userName,
+//         });
+//       });
+//     });
+//   });
+// };
 
 
 // Slick
